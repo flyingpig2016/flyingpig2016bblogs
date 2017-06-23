7ff8c8a63faa8b94dc2b680bef49f56d6d4d18ca
flyingpig2016 <flyingpig2016@163.com>
2017-06-23 13:06:20 +0800

    Site updated: 2017-06-23 13:06:19

--- js/photos.js
+++ js/photos.js
@@ -18,8 +18,8 @@
                 var html, li = "";               
                 for (var i = begin; i < end && i < data.length; i++) {
                     li += '<li><div class="img-box">' +
-                        '<a class="img-bg" rel="example_group" href="https://github.com/flyingpig2016/flyingpig2016.github.io/blob/master/img/' + data[i] + '?raw=true"></a>' +
-                        '<img lazy-src="https://github.com/flyingpig2016/flyingpig2016.github.io/blob/master/img/' + data[i] + '?raw=true" />' +
+                        '<a class="img-bg" rel="example_group" href="http://github.com/flyingpig2016/flyingpig2016.github.io/blob/master/img/' + data[i] + '?raw=true"></a>' +
+                        '<img lazy-src="http://github.com/flyingpig2016/flyingpig2016.github.io/blob/master/img/' + data[i] + '?raw=true" />' +
                         '</li>';
                 }
 
