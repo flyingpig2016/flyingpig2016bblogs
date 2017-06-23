568dd245d3cc59ef1a205a16b4ff5ca139343730
flyingpig2016 <flyingpig2016@163.com>
2017-06-23 13:07:54 +0800

    Site updated: 2017-06-23 13:07:53

--- js/photos.js
+++ js/photos.js
@@ -4,7 +4,7 @@
             offset: 20,
             init: function () {
                 var that = this;
-                $.getJSON("/photo/output.json", function (data) {
+                $.getJSON("/photos/output.json", function (data) {
                     that.render(that.page, data);
 
                     that.scroll(data);
